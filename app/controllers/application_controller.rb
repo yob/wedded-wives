e3c2361b5c58dec1fabf6b811b5a0bd28f93fa12
James Healy <jimmy@deefa.com>
2011-01-16 15:34:40 +1100

    add a basic layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :init_includes_collections
+
+  private
+
+  def init_includes_collections
+    @stylesheets = ["formtastic", "style"]
+    @javascripts = [:jquery, :rails, :application ]
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def javascripts
+    @javascripts.uniq.collect { |script|
+      javascript_include_tag(script.to_s)
+    }.join.html_safe
+  end
+
+  def stylesheets
+    @stylesheets.map { |name|
+      stylesheet_link_tag(name)
+    }.join.html_safe
+  end
 end
