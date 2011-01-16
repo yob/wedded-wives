0edbefa03ac20dc915fd66cda78c85cd319900d5
James Healy <jimmy@deefa.com>
2011-01-16 15:02:07 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weddedwives::Application.config.secret_token = 'b8f631ed01f328a937d32dffb1b3676dc96671033e4dc8328bd5df675a454039d25d3844cf1804cb3d2ef402ba501e723f8b0b7e5a07422a0bc08127acaa9249'
